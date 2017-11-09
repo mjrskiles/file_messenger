2dc70b4c4e2c747c178c008b55be91ec545d6d0d
Michael Skiles <mjrskiles@gmail.com>
2017-11-08 22:23:03 -0600

    File sending seems to work.

--- messenger_with_files.py
+++ messenger_with_files.py
@@ -7,6 +7,7 @@ Michael Skiles
 import threading
 import sys
 import os
+import struct
 import socket
 import getopt
 
@@ -55,10 +56,10 @@ def parse_opts( argv, argc ):
                 print("The server port was not specified correctly.")
                 usage( argv[0] )
 
-    print(run_as_server)
-    print(listen_port)
-    print(host)
-    print(port)
+#    print(run_as_server)
+#    print(listen_port)
+#    print(host)
+#    print(port)
 
     if listen_port == '':
         usage( argv[0] )
@@ -101,9 +102,6 @@ class Messenger:
         """
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect( (host, int(port)) )
-        if sock is None:
-            print("Could not open socket.")
-            sys.exit(1)
         return sock
 
     def accept_connection( self ):
@@ -115,6 +113,10 @@ class Messenger:
     def run_messenger( self ):
         msg_receiver = threading.Thread( target=self.get_messages )
         msg_receiver.start()
+        self.threads.append( msg_receiver )
+        file_server = threading.Thread( target=self.serve_files )
+        file_server.start()
+        self.threads.append( file_server )
         self.get_input()
 
     def get_messages( self ):
@@ -147,15 +149,97 @@ class Messenger:
         self.text_sock.send( text.encode() )
 
     def get_file( self, file_name ):
-        file_conn = threading.Thread( target=self.request_file, args=(file_name) )
+        print(type(file_name))
+        print(file_name)
+        #There needs to be a comma after file_name because args expects a tuple!!!
+        file_conn = threading.Thread( target=self.request_file, args=(file_name,) )
         self.threads.append(file_conn)
         file_conn.start()
 
     def request_file( self, file_name ):
-        file_sock = self.request_connection( self.server_host, self.server_port )
+        """Opens a connection to the file server and receives the file"""
+        try:
+            file_sock = self.request_connection( self.server_host, self.server_port )
+            if file_sock:
+                print("file_sock opened")
+            file_sock.send( file_name.encode() )
+            file_size_bytes = file_sock.recv( 4 )
+            if file_size_bytes:
+                file_size= struct.unpack( '!L', file_size_bytes[:4] )[0]
+                if file_size:
+                    Messenger.receive_file( file_sock, file_name )
+                else:
+                    print( 'File does not exist or is empty' )
+            else:
+                print( 'File does not exist or is empty' )
+        except:
+            print("Could not open connection to file server.")
+        finally:
+            if file_sock is not None:
+                file_sock.close()
+
+    def receive_file( sock, filename ):
+        """receive the file lines returned from the server"""
+        file= open( filename, 'wb' )
+        while True:
+            file_bytes= sock.recv( 1024 )
+            if file_bytes:
+                file.write( file_bytes )
+            else:
+                break
+        file.close()
+
+    def serve_files( self ):
+        print("Opening file server...")
+        while True:
+            sock, addr = self.accept_connection()
+            file_server = threading.Thread( target=Messenger.handle_request, args=(sock,) )
+            file_server.start()
+            self.threads.append(file_server)
+
+    def handle_request( sock ):
+        msg_bytes= sock.recv(1024)
+        # convert the message into a string, which is the name of the desired file
+        file_name= msg_bytes.decode()
+        # check whether the file exists; if it does, send back the file size
+        print("Received file name: {}".format(file_name))
+        try:
+            file_stat= os.stat( file_name )
+            if file_stat.st_size:
+                print("Found file {}".format(file_name))
+                file= open( file_name, 'rb' )
+                Messenger.send_file( sock, file_stat.st_size, file )
+            else:
+                print("File not found: {}".format(file_name))
+                Messenger.no_file( sock )
+        except OSError:
+            Messenger.no_file( sock )
+        finally:
+            sock.close()
+
+    def send_file( sock, file_size, file ):
+        print( 'File size is ' + str(file_size) )
+        file_size_bytes= struct.pack( '!L', file_size )
+        # send the number of bytes in the file
+        sock.send( file_size_bytes )
+        # read the file and transmit its contents
+        while True:
+            file_bytes= file.read( 1024 )
+            if file_bytes:
+                sock.send( file_bytes )
+            else:
+                break
+        file.close()
+
+    def no_file( sock ):
+        zero_bytes= struct.pack( '!L', 0 )
+        sock.send( zero_bytes )
 
     def clean_up( self ):
         self.text_sock.close()
+        self.listen_sock.close()
+        for sock in self.open_socks:
+            sock.close()
         os._exit(0)
 
 
@@ -181,6 +265,9 @@ class Client( Messenger ):
         self.server_host = host
         self.server_port = port
         self.text_sock = self.request_connection( self.server_host, self.server_port )
+        if self.text_sock is None:
+            print("Could not open socket.")
+            sys.exit(1)
         print("Connection accepted.  Sending listen port " + listen_port + ".")
         self.text_sock.send( listen_port.encode() )
         self.listen_sock = self.open_listener( listen_port )
